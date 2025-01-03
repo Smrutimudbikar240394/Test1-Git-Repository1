7c5f43a05b5cc34723343d6b5af82f9b872a3673
Smrutimudbikar240394 <smrutimudbikar240394@gmail.com>
2025-01-03 09:26:00 +0530

    Create foreach.java

---
+++ src/test/java/Report/foreach.java
@@ -0,0 +1,20 @@
+package Report;
+
+public class foreach 
+{
+	public static void main(String[] args) 
+	{
+		int[] a=new int[5];
+		a[0]=20;
+		a[1]=22;
+		a[3]=56;
+		
+		for(int w:a)
+		{
+			System.out.println(w);
+		}
+		
+		
+	}
+
+}
